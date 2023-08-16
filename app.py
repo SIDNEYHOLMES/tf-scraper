60459cecd5f2ef009fa516298a695835a31134f1
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-08-16 14:23:48 -0500

    WORKS: scraping the tf2 wiki for all weapons in order

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 
 request_results = requests.get('https://wiki.teamfortress.com/wiki/Weapons')
-wiki_page = BeautifulSoup(request_results.text, "html.parser").find('table', class_='wikitable grid').find_all('a', title=True)
+wiki_page = BeautifulSoup(request_results.text, "html.parser")
 
-print(wiki_page)
+# Find all the tables with class 'wikitable grid'
+all_tables = wiki_page.find_all('table', class_='wikitable grid')
 
-for a_tag in wiki_page:
-    title_data = a_tag['title']
-    print(title_data)
+# store unique title data in order
+unique_titles = OrderedDict()
+
+# Loop through each table and find the <a> tags with title data
+for table in all_tables:
+    wiki_links = table.find_all('a', title=True)
+    for a_tag in wiki_links:
+        title_data = a_tag['title']
+        unique_titles[title_data] = None
+
+# Print the unique title data in the order they were encountered
+for title in unique_titles.keys():
+    print(title)
